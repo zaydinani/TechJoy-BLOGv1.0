7dbc8894198f19de5231fa46c6a017772527edcd
Mohammed Marsou <marsoumtm@gmail.com>
2023-01-23 15:53:13 +0100

    test1

--- router/admin.js
+++ router/admin.js
@@ -3,7 +3,6 @@ const DB = require("../utils/database");
 const adminController = require("../controllers/adminControllers");
 
 const router = express.Router();
-//* helowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 //! dashboard ROUTES
 
 //* get  new articles router
